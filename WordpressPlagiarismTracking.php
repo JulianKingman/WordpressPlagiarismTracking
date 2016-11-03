a775d7ab4d249e1026eff9c4faec0400b2fef6d0
Julian Kingman <julian@designman.io>
2016-11-03 12:00:01 -0400

    fiddling with default taxonomies

--- WordpressPlagiarismTracking.php
+++ WordpressPlagiarismTracking.php
@@ -17,38 +17,41 @@ Plugin Type: Piklist
 // Post type templates
 // ----------------------------------------------------------------------------
 
-function wpmystics_plagiarism_case_single($template) {
+function wpmystics_plagiarism_case_single($template)
+{
     global $post;
     // Is this a "plagiarism_case" post?
-    if ($post->post_type == "plagiarism_case"){
+    if ($post->post_type == 'plagiarism_case') {
         //Your plugin path
-        $plugin_path = plugin_dir_path( __FILE__ );
+        $plugin_path = plugin_dir_path(__FILE__);
         // The name of custom post type single template
         $template_name = 'single-plagiarism_case.php';
         // A specific single template for my custom post type exists in theme folder? Or it also doesn't exist in my plugin?
-        if($template === get_stylesheet_directory() . '/' . $template_name
-            || !file_exists($plugin_path . $template_name)) {
+        if ($template === get_stylesheet_directory().'/'.$template_name
+            || !file_exists($plugin_path.$template_name)) {
             //Then return "single.php" or "single-my-custom-post-type.php" from theme directory.
             return $template;
         }
         // If not, return my plugin custom post type template.
-        return $plugin_path . $template_name;
+        return $plugin_path.$template_name;
     }
     //This is not my custom post type, do nothing with $template
     return $template;
 }
 add_filter('single_template', 'wpmystics_plagiarism_case_single');
 
-function wpmystics_plagiarism_case_archive( $archive_template ) {
-     global $post;
+function wpmystics_plagiarism_case_archive($archive_template)
+{
+    global $post;
 
-     if ( is_post_type_archive ( 'my_post_type' ) ) {
-          $archive_template = dirname( __FILE__ ) . '/post-type-template.php';
-     }
-     return $archive_template;
+    if (is_post_type_archive('my_post_type')) {
+        $archive_template = dirname(__FILE__).'/post-type-template.php';
+    }
+
+    return $archive_template;
 }
 
-add_filter( 'archive_template', 'wpmystics_plagiarism_case_archive' ) ;
+add_filter('archive_template', 'wpmystics_plagiarism_case_archive');
 
 // ----------------------------------------------------------------------------
 // Category taxonomy
@@ -56,8 +59,9 @@ add_filter( 'archive_template', 'wpmystics_plagiarism_case_archive' ) ;
 
 
 add_filter('piklist_taxonomies', 'wpmystics_register_taxonomy');
- function wpmystics_register_taxonomy($taxonomies) {
-    $taxonomies[] = array(
+ function wpmystics_register_taxonomy($taxonomies)
+ {
+     $taxonomies[] = array(
        'post_type' => 'plagiarism_case'
        ,'name' => 'case_category'
        ,'show_admin_column' => true
@@ -68,45 +72,52 @@ add_filter('piklist_taxonomies', 'wpmystics_register_taxonomy');
          ,'show_ui' => true
          ,'query_var' => true
          ,'rewrite' => array(
-           'slug' => 'case-category'
-         )
-       )
+           'slug' => 'case-category',
+         ),
+       ),
      );
-   return $taxonomies;
+
+     return $taxonomies;
  }
- add_action ( 'init', 'wpmystics_default_categories' );
+
+ add_action('init', 'wpmystics_default_categories');
  // Populate the categories when not present
- function wpmystics_default_categories(){
+ function wpmystics_default_categories()
+ {
 
     //  see if we already have populated any categories
-     $terms = get_terms ('case_category', array( 'hide_empty' => false ) );
+     $terms = get_terms('case_category', array('hide_empty' => true));
     //  if no terms then lets add the categories
-     if( empty( $terms ) ){
-     $terms = array(
-         array( 'name' => 'Complex Cases','slug' => 'complex'),
-         array( 'name' => 'Books that resemble MLP\'s works','slug' => 'resembles-mlp-works', 'parent'=>get_term_by('slug', 'complex', 'case_category')),
-         array( 'name' => 'Courses that resemble Belsebuub\'s work','slug' => 'resembles-belsebuub-works', 'parent'=>get_term_by('slug', 'complex', 'case_category')),
-         array( 'name' => 'Other copying that resembles MLP\'s works','slug' => 'resembles-other', 'parent'=>get_term_by('slug', 'complex', 'case_category')),
-         array( 'name' => 'Posted MLP works','slug' => 'posted-works'),
-         array( 'name' => 'eBook(s) posted','slug' => 'posted-ebook', 'parent'=>get_term_by('slug', 'posted-works', 'case_category')),
-         array( 'name' => 'Not readily available eBook pdfs posted','slug' => 'posted-ebook-404', 'parent'=>get_term_by('slug', 'posted-works', 'case_category')),
-         array( 'name' => 'Old Course PDFs posted','slug' => 'posted-course-pdf', 'parent'=>get_term_by('slug', 'posted-works', 'case_category')),
-         array( 'name' => 'Audio and Video','slug' => 'posted-multimedia', 'parent'=>get_term_by('slug', 'posted-works', 'case_category')),
-         array( 'name' => 'Copied Articles and Parts of Books','slug' => 'copied-parts'),
-         array( 'name' => '1 to 3 Paragraphs copied','slug' => '3-paragraphs', 'parent'=>get_term_by('slug', 'copied-parts', 'case_category')),
-         array( 'name' => 'Contacted Nothing Happened','slug' => 'contacted-nothing-happened', 'parent'=>get_term_by('slug', 'copied-parts', 'case_category')),
-         array( 'name' => '4 + Paragraphs copied','slug' => '4-plus-paragraphs', 'parent'=>get_term_by('slug', 'copied-parts', 'case_category')),
-         array( 'name' => 'Articles on Waking Times reposted – attribution not ideal','slug' => 'attribution-not-ideal', 'parent'=>get_term_by('slug', 'copied-parts', 'case_category')),
-         array( 'name' => 'Resolved – 1 to 3 Paragraphs','slug' => '3-paragraphs-resolved', 'parent'=>get_term_by('slug', 'copied-parts', 'case_category')),
-         array( 'name' => 'Resolved – 4 + paragraphs','slug' => '4-plus-paragraphs-resolved', 'parent'=>get_term_by('slug', 'copied-parts', 'case_category')),
-     	);
-         foreach( $terms as $term ){
-             if( !term_exists( $term['name'], 'case_category' ) ){
-                 wp_insert_term( $term['name'], 'case_category', array( 'slug' => $term['slug'] ) );
+     if (empty($terms)) {
+         $terms = array(
+         ['name' => 'Complex Cases','slug' => 'complex'],
+         ['name' => 'Books that resemble MLP\'s works','slug' => 'resembles-mlp-works', 'parent' => get_term_by('slug', 'complex', 'case_category')],
+         ['name' => 'Courses that resemble Belsebuub\'s work','slug' => 'resembles-belsebuub-works', 'parent' => get_term_by('slug', 'complex', 'case_category')],
+         ['name' => 'Other copying that resembles MLP\'s works','slug' => 'resembles-other', 'parent' => get_term_by('slug', 'complex', 'case_category')],
+         ['name' => 'Posted MLP works','slug' => 'posted-works'],
+         ['name' => 'eBook(s) posted','slug' => 'posted-ebook', 'parent' => get_term_by('slug', 'posted-works', 'case_category')],
+         ['name' => 'Not readily available eBook pdfs posted','slug' => 'posted-ebook-404', 'parent' => get_term_by('slug', 'posted-works', 'case_category')],
+         ['name' => 'Old Course PDFs posted','slug' => 'posted-course-pdf', 'parent' => get_term_by('slug', 'posted-works', 'case_category')],
+         ['name' => 'Audio and Video','slug' => 'posted-multimedia', 'parent' => get_term_by('slug', 'posted-works', 'case_category')],
+         ['name' => 'Copied Articles and Parts of Books','slug' => 'copied-parts'],
+         ['name' => '1 to 3 Paragraphs copied','slug' => '3-paragraphs', 'parent' => get_term_by('slug', 'copied-parts', 'case_category')],
+         ['name' => 'Contacted Nothing Happened','slug' => 'contacted-nothing-happened', 'parent' => get_term_by('slug', 'copied-parts', 'case_category')],
+         ['name' => '4 + Paragraphs copied','slug' => '4-plus-paragraphs', 'parent' => get_term_by('slug', 'copied-parts', 'case_category')],
+         ['name' => 'Articles on Waking Times reposted – attribution not ideal','slug' => 'attribution-not-ideal', 'parent' => get_term_by('slug', 'copied-parts', 'case_category')],
+         ['name' => 'Resolved – 1 to 3 Paragraphs','slug' => '3-paragraphs-resolved', 'parent' => get_term_by('slug', 'copied-parts', 'case_category')],
+         ['name' => 'Resolved – 4 + paragraphs','slug' => '4-plus-paragraphs-resolved', 'parent' => get_term_by('slug', 'copied-parts', 'case_category')],
+         );
+         foreach ($terms as $term) {
+             if (!term_exists($term['name'], 'case_category')) {
+                 wp_insert_term($term['name'],'case_category',
+                 array(
+                   'slug' => $term['slug'],
+                   'parent' => $term['parent'] ? $term['parent'] : '',
+                 )
+               );
              }
          }
      }
-
  }
 
 // ----------------------------------------------------------------------------
@@ -116,7 +127,7 @@ add_filter('piklist_taxonomies', 'wpmystics_register_taxonomy');
 add_filter('piklist_post_types', 'wpmystics_create_post_type');
   function wpmystics_create_post_type($post_types)
   {
-    $post_types['plagiarism_case'] = array(
+      $post_types['plagiarism_case'] = array(
       'labels' => piklist('post_type_labels', 'Plagiarism Case')
       ,'title' => __('Enter a new Plagiarism Case Link')
       ,'menu_icon' => 'dashicons-welcome-view-site'
@@ -126,7 +137,7 @@ add_filter('piklist_post_types', 'wpmystics_create_post_type');
         , 'editor'
         , 'custom-fields'
         , 'thumbnail'
-        , 'page-attributes'
+        , 'page-attributes',
       )
       ,'public' => true
       ,'admin_body_class' => array(
@@ -134,43 +145,41 @@ add_filter('piklist_post_types', 'wpmystics_create_post_type');
       )
       ,'has_archive' => true
       ,'rewrite' => array(
-        'slug' => 'plagiarism-case'
+        'slug' => 'plagiarism-case',
       )
       ,'capability_type' => 'post'
       ,'edit_columns' => array(
         'title' => __('Link')
-        ,'author' => __('Entered by')
+        ,'author' => __('Entered by'),
       )
       ,'hide_meta_box' => array(
-        'author'
+        'author',
       )
       ,'status' => array(
         'open' => array(
           'label' => 'Open'
-          ,'public' => true
+          ,'public' => true,
         )
         ,'in_progress' => array(
           'label' => 'In Progress'
-          ,'public' => true
+          ,'public' => true,
         )
         ,'contacted' => array(
           'label' => 'Contacted nothing happened'
-          ,'public' => true
+          ,'public' => true,
           // ,'exclude_from_search' => true
           // ,'show_in_admin_all_list' => true
           // ,'show_in_admin_status_list' => true
        )
         ,'resolved' => array(
           'label' => 'Resolved'
-          ,'public' => true
+          ,'public' => true,
         )
-        ,'resolved-comment' =>array(
-          'label'=>'Resolved - attributed via comment'
-        )
-      )
+        ,'resolved-comment' => array(
+          'label' => 'Resolved - attributed via comment',
+        ),
+      ),
     );
-    return $post_types;
+
+      return $post_types;
   }
-
-
-?>
