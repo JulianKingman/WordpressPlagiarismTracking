f84dc88dc6651d5591c3ed0d95ac0da092eab9f8
Roy Scheffers <r.scheffers@gmail.com>
2016-11-08 12:53:11 -0500

    Some more experimentation with displaying post data

--- template-open-cases.php
+++ template-open-cases.php
@@ -10,6 +10,10 @@
   margin: 20px;
   border: 1px solid #222223;
   display: inline-block;
+  background-color: #d1d3d6;
+}
+.box:hover {
+  background-color: #e8e8e8;
 }
 </style>
 
@@ -26,10 +30,24 @@ if ( $the_query->have_posts() ) :
     // Start the Loop
     while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
         <div class="box">
-        Time of the post: <?php the_time(); ?>
+        Post ID: <?php print($post->ID); ?>
+        <br> <?php echo get_the_date(); ?>, <?php the_time(); ?>
         <br> Title: <?php the_title(); ?>
         <br> Exceprt <?php the_excerpt(); ?>
         <br> Publisher: <?php the_author(); ?>
+        <br> Category: <?php 
+            //print( $post->ID);
+            var_dump( get_the_terms( $post->ID ) );
+
+            $category_detail=get_the_category( $post->ID );//$post->ID
+            var_dump($category_detail);
+            foreach($category_detail as $cd){
+            echo $cd->cat_name;
+            }
+
+        //wp_get_post_categories( get_the_ID() );
+        ?>
+
         <br> Meta data: <?php the_meta(); ?>
         <br> <?php the_shortlink('click to view'); ?>
         </div>
