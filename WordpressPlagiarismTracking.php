b4f33805b6ed4568b309a83487eab82749afadf8
Roy Scheffers <r.scheffers@gmail.com>
2016-11-08 20:11:46 -0500

    Increase version no. - First try in displaying archive plagiarism cases

--- WordpressPlagiarismTracking.php
+++ WordpressPlagiarismTracking.php
@@ -3,7 +3,7 @@
 Plugin Name: Plagiarism Administration Tools
 Plugin URI:
 Description: A simple issue tracker for tracking and following up on plagiarism
-Version: 0.0.13
+Version: 0.0.14
 Author: Mystics
 Author URI: https://github.com/JulianKingman
 License: none
