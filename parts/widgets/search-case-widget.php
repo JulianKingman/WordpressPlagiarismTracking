7d2860ee536e6022a8f9383b0dc141c53df153f8
Roy Scheffers <r.scheffers@gmail.com>
2016-11-12 01:23:43 -0500

    Clean up of widget code

--- parts/widgets/search-case-widget.php
+++ parts/widgets/search-case-widget.php
@@ -8,6 +8,6 @@ Class: wpt-quick-form-widget
 echo $before_widget;
 echo $before_title;
 echo $after_title;
-echo 'Page ID (from widget settings: '. $settings['shortcode-page'];
+//echo 'Page ID (from widget settings: '. $settings['shortcode-page'];
 echo do_shortcode('[piklist_form form="search-case-form" add_on="WordpressPlagiarismTracking"]');
 echo $after_widget;
