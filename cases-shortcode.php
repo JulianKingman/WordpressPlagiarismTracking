9f4ad07324c077376873bc11417e5e28c443e8d8
Julian Kingman <julian@designman.io>
2016-11-13 17:09:02 -0500

    more conflicts

--- cases-shortcode.php
+++ cases-shortcode.php
@@ -1,26 +1,23 @@
 <?php
 // Add Shortcode
-function cases_shortcode() {
-
-  $user_table = piklist(get_users(
+function cases_shortcode()
+{
+    $user_table = piklist(get_users(
     array(
-      'orderby' => 'display_name'
-      ,'order' => 'asc'
-    )
-    ,'objects'
-  )
-  ,array('ID' ,'display_name')
+      'orderby' => 'display_name', 'order' => 'asc',
+    ), 'objects'
+  ), array('ID', 'display_name')
   );
 
-  $link = $_GET['link'];
-  $status = $_GET['status'];
-  $cat_id = $_GET['category'];
-  $owner_id = $_GET['owner'];
-  $submitter_id = $_GET['submitter'];
+    $link = $_GET['link'];
+    $status = $_GET['status'];
+    $cat_id = $_GET['category'];
+    $owner_id = $_GET['owner'];
+    $submitter_id = $_GET['submitter'];
 
-  $owner_name = $user_table[$owner_id];
-  $submitter_name = $user_table[$submitter_id];
-?>
+    $owner_name = $user_table[$owner_id];
+    $submitter_name = $user_table[$submitter_id];
+    ?>
 <!--for debugging only
   Search options:
   <br>link: <?php //echo $link; ?>
@@ -31,27 +28,27 @@ function cases_shortcode() {
 -->
 <?php
   // -- query arguments
-  $args = array( 'post_type'    => 'plagiarism_case' ,      // only show post plagiarism_case data
-                 'orderby'      => 'date',
-                 'order'        => 'ASC',                   // sort on date, olders to newest
-                 'meta_key'     => 'assigned_user',         // optional filter: Owner
-                 'title'        => $link,                   // optional filter for links
-                 'post_status'  => $status,                 // optional filter for status
-                 'meta_value'   => $owner_id,               // owner value
-                 'author'       => $submitter_id,            // optional filter for submitter
+  $args = array('post_type' => 'plagiarism_case' ,      // only show post plagiarism_case data
+                 'orderby' => 'date',
+                 'order' => 'ASC',                   // sort on date, olders to newest
+                 'meta_key' => 'assigned_user',         // optional filter: Owner
+                 'title' => $link,                   // optional filter for links
+                 'post_status' => $status,                 // optional filter for status
+                 'meta_value' => $owner_id,               // owner value
+                 'author' => $submitter_id,            // optional filter for submitter
   );
-  if ( $cat_id != '' ){
-            $args['tax_query']  =  array(array(
+    if ($cat_id != '') {
+        $args['tax_query'] = array(array(
                                     'taxonomy' => 'case_category',
-                                    'field'    => 'term_id',
-                                    'terms'    => $cat_id
+                                    'field' => 'term_id',
+                                    'terms' => $cat_id,
                                    ));
-  };
+    };
 
   // Variable to call WP_Query.
-  $the_query = new WP_Query( $args );
+  $the_query = new WP_Query($args);
   //$the_query = new WP_Query( );
-  if ( $the_query->have_posts() ) :
+  if ($the_query->have_posts()) :
 
   // Start the Loop
 ?>
@@ -67,55 +64,63 @@ function cases_shortcode() {
     <th>Submitter</th>
     <th>Go</th>
   </tr>
-  </thead>  
-  <?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
-<<<<<<< HEAD
-    <tr>
-=======
+  </thead>
   <tbody>
-    <tr>  
->>>>>>> d09a8bdf85c9ee4ced8afe61142cb381ddcfc124
+  <?php while ($the_query->have_posts()) : $the_query->the_post();
+    ?>
+    <tr>
       <td><?php echo get_the_ID() ?></td>
       <!-- Status -->
-      <td><?php echo get_post_status( $post->ID ); ?></td>
+      <td><?php echo get_post_status($post->ID);
+    ?></td>
 
       <!-- Owner -->
-      <td><?php echo $user_table[get_post_meta(get_the_ID(), 'assigned_user', true)]; ?></td>
+      <td><?php echo $user_table[get_post_meta(get_the_ID(), 'assigned_user', true)];
+    ?></td>
 
       <!-- Category -->
-      <td><?php $cats = get_the_terms( $post->ID, 'case_category'); ?>
+      <td><?php $cats = get_the_terms($post->ID, 'case_category');
+    ?>
         <ul>
-          <?php if ($cats == false) echo '<li>none</li>';
-          foreach($cats as $cat) {
-            echo '<li>' . $cat->name . '</li>';
-          } ?>
+          <?php if ($cats == false) {
+    echo '<li>none</li>';
+}
+    foreach ($cats as $cat) {
+        echo '<li>'.$cat->name.'</li>';
+    }
+    ?>
         </ul>
       </td>
 
       <!-- Links, copied and original -->
       <td>
-        COPIED: <?php the_title(); ?><br>
+        COPIED: <?php the_title();
+    ?><br>
         ORIGINAL: <?php echo get_post_meta(get_the_ID(), 'original', true) ?>
       </td>
 
       <!-- Date -->
-      <td><?php echo get_the_date('M j, Y'); ?> @ <?php the_time(); ?></td>
+      <td><?php echo get_the_date('M j, Y');
+    ?> @ <?php the_time();
+    ?></td>
 
       <!-- Author -->
-      <td><?php the_author(); ?></td>
-      <td><?php the_shortlink('view'); ?></td>
+      <td><?php the_author();
+    ?></td>
+      <td><?php the_shortlink('view');
+    ?></td>
     </tr>
-  </tbody>
   <?php     // End the Loop
   endwhile;
-  wp_reset_postdata();
-  else :
+    wp_reset_postdata(); else :
   // If no posts match this query, output this text.
-  _e( 'Sorry, no posts matched your criteria.', 'textdomain' );
-  endif;
-  ?>
+  _e('Sorry, no posts matched your criteria.', 'textdomain');
+    endif;
+    ?>
+</tbody>
   </table>
   <?php
+
 }
-add_shortcode( 'plagiarism_cases', 'cases_shortcode' );
+add_shortcode('plagiarism_cases', 'cases_shortcode');
 ?>
