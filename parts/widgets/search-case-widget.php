aa151e60b5943f62e51f84821a25b1936e9ba576
Roy Scheffers <r.scheffers@gmail.com>
2016-11-15 14:19:36 -0500

    Add widget page setting in a div

--- parts/widgets/search-case-widget.php
+++ parts/widgets/search-case-widget.php
@@ -4,12 +4,14 @@ Title: Search Case Widget
 Description: Search Plagiarism cases
 Class: wpt-quick-form-widget
 */
-
+?>
+<div id="wpt-search-shortcode-wrapper" data-url="<?php echo get_the_permalink( $settings['shortcode-page'] ); ?>"></div>
+<?php
 echo $before_widget;
 echo $before_title;
 echo $after_title;
-
 echo '<h3>Search</h3>';
 //echo 'Page ID (from widget settings: '. $settings['shortcode-page'];
 echo do_shortcode('[piklist_form form="search-case-form" add_on="WordpressPlagiarismTracking"]');
 echo $after_widget;
+?>
